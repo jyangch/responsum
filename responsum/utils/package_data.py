08898b285910ffe0dfe188c609d627dc7d14c90d
Jun Yang <84176695+jyangch@users.noreply.github.com>
2025-10-27 16:49:20 +0800

    Replace pkg_resources with importlib.resources

--- responsum/utils/package_data.py
+++ responsum/utils/package_data.py
@@ -1,12 +1,13 @@
-import pkg_resources
+import importlib.resources as resources
 import os
 from shutil import copyfile
 
 
 def get_path_of_data_dir():
-    file_path = pkg_resources.resource_filename("responsum", "data")
 
-    return file_path
+    path = resources.files("responsum") / "data"
+
+    return str(path)
 
 
 def get_path_of_data_file(data_file):
